011e23d6a9a9fc503f3cb9bfffe4fc1d837897c8
wischawis <wis-chawis@hotmail.com>
2018-08-06 17:38:28 +0700

    Update BOT01.php

--- BOT01.php
+++ BOT01.php
@@ -24,7 +24,7 @@
 	$arrayPostData['messages'][0]['body']['contents'][0]['size'] = "md";
 	$arrayPostData['messages'][0]['body']['contents'][0]['align'] = "center";
 	$arrayPostData['messages'][0]['body']['contents'][0]['color'] = "#ff0000";
-    replyMsg($arrayHeader,$arrayPostData);
+    replyMsgFlex($arrayHeader,$arrayPostData);
 
 #ตัวอย่าง Message Type "Text"
     if($message == "สวัสดี"){
@@ -72,6 +72,19 @@
     }
 function replyMsg($arrayHeader,$arrayPostData){
         $strUrl = "https://api.line.me/v2/bot/message/reply";
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL,$strUrl);
+        curl_setopt($ch, CURLOPT_HEADER, false);
+        curl_setopt($ch, CURLOPT_POST, true);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $arrayHeader);    
+        curl_setopt($ch, CURLOPT_POSTFIELDS,json_encode($arrayPostData));
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER,true);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        $result = curl_exec($ch);
+        curl_close ($ch);
+    }
+function replyMsgFlex($arrayHeader,$arrayPostData){
+        $strUrl = "https://api.line.me/v2/bot/message/push";
         $ch = curl_init();
         curl_setopt($ch, CURLOPT_URL,$strUrl);
         curl_setopt($ch, CURLOPT_HEADER, false);
